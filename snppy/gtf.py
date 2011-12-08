afae8f41bfa0db71da5411419f0dd91264e89f2c
Tobias Sargeant <tobias.sargeant@gmail.com>
2011-12-08 11:37:55 -0500

    Fix ranges and gtf files.

--- snppy/gtf.py
+++ snppy/gtf.py
@@ -1,5 +1,6 @@
 from snppy.range import *
 import re
+import os
 
 
 
@@ -10,7 +11,7 @@ class GFFRecord(object):
 
   @property
   def range(self):
-    return snppy.Range1D(self.start, self.end)
+    return Range((self.start, self.end))
 
   def length(self):
     return self.end - self.start
@@ -23,7 +24,7 @@ class GFFRecord(object):
     self.feature = line[2]
     self.start = int(line[3]) - 1
     self.end = int(line[4])
-    self.score = None if line[5] == '.' else int(line[5])
+    self.score = None if line[5] == '.' else float(line[5])
     self.strand = line[6]
     self.frame = None if line[7] == '.' else int(line[7])
     self.group = line[8]
@@ -37,37 +38,43 @@ class GTFRecord(GFFRecord):
 
   def __init__(self, line):
     super(GTFRecord, self).__init__(line)
-    self.attrs = [ attr.split(None, 1) for attr in self.group.split('; ') ]
+    self.attrs = [ attr.split(None, 1) for attr in self.group.split('; ') if len(attr.strip()) ]
     self.gene_id = self.attrs[0][1].strip('"')
     self.transcript_id = self.attrs[1][1].strip('"')
+
+
+
+def readrows(inf, rectype):
+  if type(inf) in (str, unicode):
+    ext = os.path.splitext(inf)[1].lower()
+    if ext == '.gz':
+      import gzip
+      inf = gzip.open(inf)
+    elif ext == '.bz2':
+      import bz2
+      inf = bz2.BZ2File(inf)
+    else:
+      inf = open(inf)
+
+  while 1:
+    line = inf.readline()
+    if line == '':
+      break
+    yield rectype(line)
 
 
 
 class GFFFile(object):
   @classmethod
   def read(cls, inf):
-    if type(inf) in (str, unicode):
-      inf = open(inf)
-  
-    while 1:
-      line = inf.readline()
-      if line == '':
-        break
-      yield GFFRecord(line)
+    return readrows(inf, GFFRecord)
 
 
 
 class GTFFile(object):
   @classmethod
   def read(cls, inf):
-    if type(inf) in (str, unicode):
-      inf = open(inf)
-  
-    while 1:
-      line = inf.readline()
-      if line == '':
-        break
-      yield GTFRecord(line)
+    return readrows(inf, GTFRecord)
 
 
 
--- snppy/range.py
+++ snppy/range.py
@@ -2,11 +2,16 @@ class Range(object):
   __slots__ = ( 'extents', )
 
   def __str__(self):
-    return str(tuple([ _[0] for _ in self.extents])) + \
-        '-' + \
-        str(tuple([ _[1] for _ in self.extents]))
+    a, b = zip(*self.extents)
+    if len(a) == 1: a, b = a[0], b[0]
+    return repr(a) + '-' + repr(b)
 
-  def __init__(self, extents):
+  def __repr__(self):
+    a, b = zip(*self.extents)
+    if len(a) == 1: a, b = a[0], b[0]
+    return '{range:' + repr(a) + '-' + repr(b) + '}'
+
+  def __init__(self, *extents):
     self.extents = extents
 
   def overlaps(self, other):
@@ -15,20 +20,27 @@ class Range(object):
         return False
     return True
 
+  def distance(self, other):
+    def distance1(a, b):
+      if a[0] >= b[1]: return a[0] - b[1]
+      if b[0] >= a[1]: return b[0] - a[1]
+      return 0
+    return [ distance1(a, b) for a, b in zip(self.extents, other.extents) ]
+    
   def intersection(self, other):
     def _intersection(a, b):
       lo = max(a[0], b[0])
       hi = max(lo, min(a[1], b[1]))
       return (lo, hi)
 
-    return Range([ _intersection(a, b) for a, b in zip(self.extents, other.extents) ])
+    return Range(*[ _intersection(a, b) for a, b in zip(self.extents, other.extents) ])
 
   @classmethod
   def bounds(cls, ranges):
     def _bounds(ranges):
       return (min([ r[0] for r in ranges ]),
               max([ r[1] for r in ranges ]))
-    return Range([ _bounds(ranges) for ranges in zip(*[ range.extents for range in ranges ]) ])
+    return Range(*[ _bounds(ranges) for ranges in zip(*[ range.extents for range in ranges ]) ])
 
 
 
