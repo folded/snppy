d99cdfb8da3ce0bc9cc86c7f5db6ad06b5602267
Tobias Sargeant <tobias.sargeant@gmail.com>
2012-03-15 15:27:44 -0400

    Add 1D union and intersection between pairs of ordered ranges.

--- snppy/range.py
+++ snppy/range.py
@@ -81,7 +81,158 @@ def bounds(ranges):
 
 
 
-__all__ = (
+def union(r1, r2):
+  a = []
+  for r in r1: a.extend([ (r.extents[0][0], -1), (r.extents[0][1], +1) ])
+  for r in r2: a.extend([ (r.extents[0][0], -1), (r.extents[0][1], +1) ])
+  a.sort()
+  out = []
+  s = 0
+  for p, d in a:
+    s2 = s - d
+    if (s == 0 and s2 == 1) or (s == 1 and s2 == 0):
+      out.append(p)
+    s = s2
+
+  result = [ Range((x,y)) for x, y in zip(out[0::2], out[1::2]) ]
+
+  return result
+
+
+
+def intersection_1(r1, r2):
+  i = j = 0
+  out = []
+  s = 0
+  while i < len(r1)*2 or j < len(r2)*2:
+    if (j == len(r2)*2 or
+        (i != len(r1)*2 and
+         (
+          (r1[i>>1].extents[0][i&1] < r2[j>>1].extents[0][j&1]) or
+          (r1[i>>1].extents[0][i&1] == r2[j>>1].extents[0][j&1] and not i&1)
+         ))):
+      s2 = s + (-1 if (i&1) else +1)
+      p = r1[i>>1].extents[0][i&1]
+      i += 1
+    else:
+      s2 = s + (-1 if (j&1) else +1)
+      p = r2[j>>1].extents[0][j&1]
+      j += 1
+    if (s == 1 and s2 == 2) or (s == 2 and s2 == 1):
+      out.append(p)
+    s = s2
+
+  result = [ Range((x,y)) for x, y in zip(out[0::2], out[1::2]) ]
+
+  return result
+
+
+
+def intersection_2(r1, r2):
+  a = []
+  for r in r1: a.extend([ (r.extents[0][0], -1), (r.extents[0][1], +1) ])
+  for r in r2: a.extend([ (r.extents[0][0], -1), (r.extents[0][1], +1) ])
+  a.sort()
+  out = []
+  s = 0
+  for p, d in a:
+    s2 = s - d
+    if (s == 1 and s2 == 2) or (s == 2 and s2 == 1):
+      out.append(p)
+    s = s2
+
+  result = [ Range((x,y)) for x, y in zip(out[0::2], out[1::2]) ]
+
+  return result
+
+
+
+def intersection_3(r1, r2):
+  a1 = []
+  a2 = []
+  for r in r1: a1.extend([ (r.extents[0][0], -1), (r.extents[0][1], +1) ])
+  for r in r2: a2.extend([ (r.extents[0][0], -1), (r.extents[0][1], +1) ])
+
+  out = []
+  s = 0
+  i = j = 0
+  while i < len(a1) or j < len(a2):
+    if j == len(a2) or (i < len(a1) and a1[i] < a2[j]):
+      p, d = a1[i]
+      i += 1
+    else:
+      p, d = a2[j]
+      j += 1
+    s2 = s - d
+    if (s == 1 and s2 == 2) or (s == 2 and s2 == 1):
+      out.append(p)
+    s = s2
+
+  result = [ Range((x,y)) for x, y in zip(out[0::2], out[1::2]) ]
+
+  return result
+
+
+
+
+import unittest
+
+
+class TestRange(unittest.TestCase):
+  def test_union(self):
+    r1 = [ Range((0,2)) ]
+    r2 = [ Range((1,3)) ]
+    print union(r1, r2)
+
+    r1 = [ Range((0,2)) ]
+    r2 = [ Range((2,4)) ]
+    print union(r1, r2)
+
+    r1 = [ Range((0,2)) ]
+    r2 = [ Range((3,5)) ]
+    print union(r1, r2)
+
+    r1 = [ Range((0,2)), Range((3,5)) ]
+    r2 = [ Range((1,4)) ]
+    print union(r1, r2)
+
+    r1 = [ Range((0,2)), Range((3,5)) ]
+    r2 = [ Range((-2,-1)), Range((4,6)) ]
+    print union(r1, r2)
+
+    r1 = [ Range((-2,-1)), Range((0,2)), Range((3,6)) ]
+    r2 = [ Range((-1,5)) ]
+    print union(r1, r2)
+
+intersection = intersection_3
+
+__all__ = [
   'Range',
-  'bounds'
-)
+  'bounds',
+  'union',
+  'intersection'
+]
+
+
+if __name__ == '__main__':
+  import random
+
+  def test(r1, r2, rout):
+    sa = set()
+    sb = set()
+    sout = set()
+    for r in r1:
+      sa.update(range(r.extents[0][0], r.extents[0][1]))
+    for r in r2:
+      sb.update(range(r.extents[0][0], r.extents[0][1]))
+    for r in rout:
+      sout.update(range(r.extents[0][0], r.extents[0][1]))
+    assert sa & sb == sout
+
+  for i in range(10000):
+    r1 = sorted(random.sample(xrange(100000), 1000))
+    r2 = sorted(random.sample(xrange(100000), 1000))
+    r1 = [ Range((x,y)) for x, y in zip(r1[0::2], r1[1::2]) ]
+    r2 = [ Range((x,y)) for x, y in zip(r2[0::2], r2[1::2]) ]
+    rout = intersection_3(r1, r2)
+    # test(r1, r2, rout)
